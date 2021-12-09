508d19a5cf1016c7c44e06d3e385053b82f2fbef
EC2 Default User <ec2-user@ip-172-31-37-137.ec2.internal>
2021-12-09 02:25:44 +0000

    ruby-lesson7-2

---
+++ lesson7-2.rb
@@ -0,0 +1,31 @@
+puts "計算を始めます"
+puts "何回繰り返しますか？"
+a = gets.to_i
+puts "1回目の計算"
+puts "2つの値を入力してください"
+b = gets.to_i
+c = gets.to_i
+puts "b+c=#{b+c}"
+puts "b-c=#{b-c}"
+puts "b*c=#{b*c}"
+puts "b/c=#{b/c}"
+
+puts "2回目の計算"
+puts "2つの値を入力してください"
+d = gets.to_i
+e = gets.to_i
+puts "d+e=#{d+e}"
+puts "d-e=#{d-e}"
+puts "d*e=#{d*e}"
+puts "d/e=#{d/e}"
+
+puts "3回目の計算"
+puts "2つの値を入力してください"
+f = gets.to_i
+g = gets.to_i
+puts "f+g=#{f+g}"
+puts "f-g=#{f-g}"
+puts "f*g=#{f*g}"
+puts "f/g=#{f/g}"
+
+puts "計算を終了します"
